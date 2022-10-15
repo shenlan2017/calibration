8f5a86bf9f413007172478f49440d65539b1b892
ChenJiahao031008 <ChenJiahao031008@outlook.com>
2022-10-15 18:13:57 +0800

    cmake 3.15 -> 3.10

---
+++ data_processor_ws/cyber2ros_ros_ws/src/apollo2ros/build/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "message_runtime;message_generation;roscpp;sensor_msgs;cv_bridge".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "apollo2ros"
+PROJECT_SPACE_DIR = "/home/t/Desktop/calibration/data_processor_ws/cyber2ros_ros_ws/src/apollo2ros/build/devel"
+PROJECT_VERSION = "0.0.0"
